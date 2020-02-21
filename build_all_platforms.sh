2dd1985574dfe01d20f26ea72440d3be7ff64bc4
Anton Krug <anton.krug@microchip.com>
2020-02-21 15:46:02 +0000

    Building Win/Lin

--- build_all_platforms.sh
+++ build_all_platforms.sh
@@ -6,8 +6,9 @@ set -o pipefail
 echo
 
 echo "Go version and enviroment"
-#go version
-#go env
+go version
+go env
+gcc --version
 echo
 
 echo "Supported distributions:"
@@ -15,15 +16,15 @@ echo "Supported distributions:"
 echo
 
 echo "Installing depedencies"
-# go get github.com/logrusorgru/aurora
-# go get github.com/dustin/go-humanize
-# go get gonum.org/v1/plot
-# go get gonum.org/v1/plot/plotter
-# go get gonum.org/v1/plot/plotutil
-# go get gonum.org/v1/plot/vg
-#go get github.com/google/gxui
-#go get github.com/google/gxui/drivers/gl
-#go get github.com/google/gxui/themes/dark
+go get github.com/logrusorgru/aurora
+go get github.com/dustin/go-humanize
+go get gonum.org/v1/plot
+go get gonum.org/v1/plot/plotter
+go get gonum.org/v1/plot/plotutil
+go get gonum.org/v1/plot/vg
+go get github.com/google/gxui
+go get github.com/google/gxui/drivers/gl
+go get github.com/google/gxui/themes/dark
 
 
 TS="${TS:-`date +"%Y%m%d-%H%M%S"`}" 
@@ -35,24 +36,24 @@ rm -r ./release
 mkdir -p release
 
 echo "Building Windows"
-GOOS=windows GOARCH=amd64 go build -o release/scplot-windows-x86-64.exe
-#GOOS=windows GOARCH=amd64 go build -o release/scplot-windows-x86-64-$TS.exe
-# GOOS=windows GOARCH=386 go build -o release/scplot-windows-x86-32-$TS.exe
-# GOOS=windows GOARCH=arm go build -o release/scplot-windows-arm-$TS.exe
+#GOOS=windows GOARCH=amd64 go build -o release/scplot-windows-x86-64.exe
+GOOS=windows GOARCH=amd64 go build -o release/scplot-windows-x86-64-$TS.exe
+#GOOS=windows GOARCH=386 go build -o release/scplot-windows-x86-32-$TS.exe
+#GOOS=windows GOARCH=arm go build -o release/scplot-windows-arm-$TS.exe
 
-# echo "Building Linux"
-# GOOS=linux GOARCH=amd64 go build -o release/scplot-linux-x86-64-$TS
-# GOOS=linux GOARCH=386 go build -o release/scplot-linux-x86-32-$TS
-# GOOS=linux GOARCH=arm64 go build -o release/scplot-linux-arm-64-$TS
-# GOOS=linux GOARCH=arm go build -o release/scplot-linux-arm-32-$TS
+echo "Building Linux"
+GOOS=linux GOARCH=amd64 go build -o release/scplot-linux-x86-64-$TS
+#GOOS=linux GOARCH=386 go build -o release/scplot-linux-x86-32-$TS
+#GOOS=linux GOARCH=arm64 go build -o release/scplot-linux-arm-64-$TS
+#GOOS=linux GOARCH=arm go build -o release/scplot-linux-arm-32-$TS
 
-# echo "Building FreeBSD"
-# GOOS=freebsd GOARCH=amd64 go build -o release/scplot-freebsd-x86-64-$TS
-# GOOS=freebsd GOARCH=386 go build -o release/scplot-freebsd-x86-32-$TS
+#echo "Building FreeBSD"
+#GOOS=freebsd GOARCH=amd64 go build -o release/scplot-freebsd-x86-64-$TS
+#GOOS=freebsd GOARCH=386 go build -o release/scplot-freebsd-x86-32-$TS
 
-# echo "Building MacOS"
-# GOOS=darwin GOARCH=amd64 go build -o release/scplot-macos-x86-64-$TS
-# GOOS=darwin GOARCH=386 go build -o release/scplot-macos-x86-32-$TS
+#echo "Building MacOS"
+#GOOS=darwin GOARCH=amd64 go build -o release/scplot-macos-x86-64-$TS
+#GOOS=darwin GOARCH=386 go build -o release/scplot-macos-x86-32-$TS
 
 echo
 echo "Finished artifacts:"
